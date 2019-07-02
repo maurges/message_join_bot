8908870988917dcfec8c9620bf3801a104ee0013
d86leader <d86leader@mail.com>
2019-07-02 14:49:03 +0700

    initial bot code

---
+++ count.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+"""
+Usage:
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+import decide
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Handler
+from telegram import Update
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def describe(obj):
+    print(f"type: {type(obj)}")
+    print(f"dir: {dir(obj)}")
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(bot, update):
+    """Send a message when the command /describe is issued."""
+    update.message.reply_text('describe!')
+
+
+def reply(counter):
+    def internal(bot, update):
+        decision = counter.process(update.message)
+
+        message = ""
+        if isinstance(decision, decide.DoNothing):
+            message = "nothing"
+        elif isinstance(decision, decide.Waited):
+            message = (f"waited {decision.seconds} seconds" +
+                       f" for {decision.messages} messages")
+
+        update.message.reply_text(message)
+    return internal
+
+
+def error(bot, update):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main(token):
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(token)
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, reply(decide.MessageCounter())))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    with open("token.txt", "r") as tfile:
+        token = tfile.read().strip()
+    main(token)
---
+++ decide.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from typing import *
+from collections import namedtuple
+from datetime import datetime
+from SortedCollection import SortedCollection
+
+
+UID = namedtuple("UID", ["chat_id", "from_id"])
+
+MessageQueue = Dict[UID, SortedCollection]
+
+
+# actions returned to caller:
+class Action:
+    pass
+class DoNothing(Action):
+    pass
+class Waited(Action):
+    def __init__(self, seconds, messages):
+        self.seconds = seconds
+        self.messages = messages
+
+
+class MessageCounter:
+    def __init__(self, base_queue : MessageQueue = {}) -> None:
+        self.msg_queue = base_queue
+
+    def process(self, message) -> Action:
+        chat_id = message.chat.id
+        from_id = message.from_user.id
+        time    = message.date
+        if not chat_id or not from_id or not time:
+            return DoNothing()
+        user_id = UID(chat_id=chat_id, from_id=from_id)
+        #
+        if user_id not in self.msg_queue:
+            self.msg_queue[user_id] = SortedCollection([time])
+            return DoNothing()
+        #
+        oldest = self.msg_queue[user_id][0]
+        self.msg_queue[user_id].insert(time)
+        diff = time - oldest
+        message_amount = len(self.msg_queue[user_id])
+        #
+        return Waited(diff.seconds, message_amount)
