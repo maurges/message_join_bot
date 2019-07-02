0522b38e0122f6e6aba78fef146d349014c37943
d86leader <d86leader@mail.com>
2019-07-03 01:08:24 +0700

    wrote test for new feature

--- test/decide_test.py
+++ test/decide_test.py
@@ -35,18 +35,28 @@ class TestDecide(unittest.TestCase):
     def test_returns_nothing(self):
         counter = logic.MessageCounter()
 
-
         msg = SimpleMessage.gen()
         r = counter.decide(msg)
-        self.assertTrue(isinstance(r, logic.DoNothing))
+        self.assertIsInstance(r, logic.DoNothing)
 
         msg.chat.id += 1
         r = counter.decide(msg)
-        self.assertTrue(isinstance(r, logic.DoNothing))
+        self.assertIsInstance(r, logic.DoNothing)
 
         msg.from_user.id += 1
         r = counter.decide(msg)
-        self.assertTrue(isinstance(r, logic.DoNothing))
+        self.assertIsInstance(r, logic.DoNothing)
+
+    def test_inserts_five(self):
+        counter = logic.MessageCounter()
+
+        msg = SimpleMessage.gen()
+        for _ in range(logic.MessageThreshold):
+            r = counter.decide(msg)
+
+        self.assertIsInstance(r, logic.InsertInto)
+        sources = r.sources
+        self.assertEqual(len(sources), logic.MessageThreshold - 1)
 
 
 if __name__ == '__main__':
