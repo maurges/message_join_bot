b2abadc1b38b98e8cb35ec6c687b67fc33357904
d86leader <d86leader@mail.com>
2019-07-02 23:45:05 +0700

    rewrote logic, added needed features

--- logic.py
+++ logic.py
@@ -2,8 +2,87 @@
 
 from typing import *
 from collections import namedtuple
-from datetime import datetime
+from datetime import datetime, timedelta
 from sortedcollection import SortedCollection
+
+"""
+Description: a class to decide when to delete which messages.
+The rule to delete is simple: if in 5 seconds we received 5 messages,
+delete and join them into one new message. Then each new messages from the same
+user will be appended to the message. This stops if the user hasn't sent
+anything in last 5 seconds.
+
+Usage: create an instance of MessageCounter and call decide() method,
+and it will tell you what to do. The method is highly stateful. It's best if
+it's called on all arriving messages.
+"""
+
+
+DelayDelete = timedelta(seconds=5)
+DelayRelease = timedelta(seconds=5)
+MessageThreshold = 5
+
+
+# For each user in chat we keep their status
+class UserStatus:
+    def update(self, message) -> 'UserStatus':
+        return self
+    def is_strict(self) -> bool:
+        return False
+    def is_lax(self) -> bool:
+        return True
+
+class UserLax(UserStatus):
+    "User allowed to post messages"
+    "This carries a payload of recent posted messages"
+
+    def __init__(self, initial_message):
+        self.queue = new_queue(initial_message)
+
+    def update(self, message) -> UserStatus:
+        # insert the new message
+        self.queue.insert(message)
+
+        # drop all old messages
+        latest_time = self.queue[-1].date
+        threshold_time = latest_time - DelayDelete
+
+        while self.queue[0].date > threshold_time:
+            self.queue.drop_index(0)
+
+        # if the queue has too much late messages
+        if len(self.queue) > MessageThreshold:
+            # we return a new status
+            return UserStrict(self.queue[-1])
+        else:
+            return self
+
+    def is_lax(self) -> bool:
+        return True
+
+class UserStrict(UserStatus):
+    "User has their messages instantly deleted"
+    "Payload is time when to stop deletion"
+
+    def __init__(self, initial_message):
+        self.base_message = initial_message
+        self.stop_time = message.date + DelayRelease
+
+    def update(self, message) -> UserStatus:
+        # update stop time
+        stop_time = self.stop_time
+        self.stop_time = max(self.stop_time, message.date + DelayRelease)
+
+        if message.date <= stop_time:
+            # still should delete the message
+            return self
+        else:
+            return UserLax(message)
+
+
+
+    def is_strict(self) -> bool:
+        return True
 
 
 # a complex key for our table
@@ -15,7 +94,7 @@ def new_queue(item):
     key = lambda x: x.date
     return SortedCollection([item], key)
 
-MessageQueue = Dict[UID, SortedCollection]
+UserCollection = Dict[UID, UserStatus]
 
 
 # actions returned to caller:
@@ -23,14 +102,14 @@ class Action:
     pass
 class DoNothing(Action):
     pass
-class Waited(Action):
-    def __init__(self, seconds, messages):
-        self.seconds = seconds
-        self.messages = messages
+class InsertInto(Action):
+    def __init__(self, receiver, source):
+        self.receiver = receiver
+        self.source = source
 
 
 class MessageCounter:
-    def __init__(self, base_queue : MessageQueue = {}) -> None:
+    def __init__(self, base_queue : UserCollection = {}) -> None:
         self.msg_queue = base_queue
 
     def decide(self, message) -> Action:
@@ -43,14 +122,15 @@ class MessageCounter:
         user_id = UID(chat_id=chat_id, from_id=from_id)
 
         if user_id not in self.msg_queue:
-            self.msg_queue[user_id] = new_queue(message)
+            self.msg_queue[user_id] = UserLax(message)
             return DoNothing()
 
-        # remove all old items
+        new_status = self.msg_queue[user_id].update(message)
+        self.msg_queue[user_id] = new_status
 
-        oldest = self.msg_queue[user_id][0]
-        self.msg_queue[user_id].insert(message)
-        diff = time - oldest.date
-        message_amount = len(self.msg_queue[user_id])
-
-        return Waited(diff.seconds, message_amount)
+        if new_status.is_lax():
+            return DoNothing()
+        elif isinstance(new_status, UserStrict):
+            return InsertInto(new_status.base_message, message)
+        else:
+            return DoNothing()
